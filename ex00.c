35c80b64493609fa8c089df31c3674bff72114fe
sofipilo <158043282+sofipilo@users.noreply.github.com>
2024-01-29 15:47:53 +0000

    .

---
+++ ex00.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    short i = -32767;
+    printf("ciao %d\n", i);
+    return(0);
+}
---
+++ ex01.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int numero;
+    int numero2;
+    int numero3;
+    int numero4;
+    int numero5;
+
+    printf("inserisci l'età di greta\n");
+    scanf("%d", &numero);
+    printf("inserisci l'età di marco\n");
+    scanf("%d", &numero2);
+    printf("inserisci l'età di lorenzo\n");
+    scanf("%d", &numero3);
+    printf("inserisci l'età di aurora\n");
+    scanf("%d", &numero4);
+    printf("inserisci l'età di sofia\n");
+    scanf("%d", &numero5);
+    printf("il tuo risultato è: %d\n", numero + numero2 + numero3 + numero4 + numero5);
+}
